b5408f1821e0c0892673f0902f0e87941fc4c760
lavi02 <hoil.jeong@fleetune.com>
2024-01-05 10:02:34 +0900

    [ADD] lhci

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,30 @@ import Myprofile from "./Myprofile";
 import Qdetail from "./Qdetail";
 
 function App() {
-    return (
-        <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Main />} />
-                    <Route path="/Myprofile" element={<Myprofile />} />
-                    <Route path="/Qdetail" element={<Qdetail />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    )
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/profile" element={<Myprofile />} />
+          <Route path="/detail" element={<Qdetail />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("SW registered: ", registration);
+      })
+      .catch((registrationError) => {
+        console.log("SW registration failed: ", registrationError);
+      });
+  });
+}
+
 export default App;
