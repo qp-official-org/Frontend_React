eee7b191c25e89cbc4366acbadbc7d470acb799f
kojesung <90428805+kojesung@users.noreply.github.com>
2024-01-05 13:49:34 +0900

    [ADD] CI/CD (#6)
    * [ADD] lhci
    
    * [FIX] CI inactive
    
    * [ADD] refactor
    
    * [ADD] refactor
    
    * [FIX] directory fix
    
    * [FIX] trigger
    
    ---------
    
    Co-authored-by: lavi02 <hoil.jeong@fleetune.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./Main";
-import Myprofile from "./Myprofile";
-import Qdetail from "./Qdetail";
+import Main from "src/Main";
+import Myprofile from "src/Myprofile";
+import Qdetail from "src/Qdetail";
 
 function App() {
-    return (
-        <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Main />} />
-                    <Route path="/Myprofile" element={<Myprofile />} />
-                    <Route path="/Qdetail" element={<Qdetail />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    )
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/profile" element={<Myprofile />} />
+          <Route path="/detail" element={<Qdetail />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("SW registered: ", registration);
+      })
+      .catch((registrationError) => {
+        console.log("SW registration failed: ", registrationError);
+      });
+  });
+}
+
 export default App;
