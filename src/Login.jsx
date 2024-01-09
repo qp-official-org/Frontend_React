12b5455c732eef2a3a132dba3372455f4742240d
Baetato <diana021015@gmail.com>
2024-01-09 19:44:00 +0900

    Login

---
+++ src/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function Login() {
+    return (
+        <div>
+            Login
+        </div>
+    )
+}
+export default Login;
