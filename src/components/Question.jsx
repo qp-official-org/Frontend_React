b4ad98b5f6d4f5432044af90a0675f2a4138589d
kojesung <90428805+kojesung@users.noreply.github.com>
2024-01-28 21:38:26 +0900

    Feature/q page (#16)
    * [ADD] 상세 설명 디자인 추가
    
    * [FIX] css
    
    * [ADD]Q&A box
    
    * [ADD] 질문 상세 디자인 추가
    
    * 질문 컨테이너 구조 제작
    
    * [ADD]디자인 추가
    
    * [FIX]Dropdown fix
    
    * [ADD]질문페이지 컴포넌트 분리 및 디자인 추가
    
    * [ADD]답변 박스 디자인 추가
    
    * [ADD]질문내용 컴포넌트 분리, 디자인 추가
    
    * [FIX]답변 입력칸 수정
    
    * [FIX]question container
    
    * [ADD]답변 블러처리 및 간격 수정
    
    * [ADD]등록버튼 추가 및 드롭다운 컴포넌트 분리
    
    * test
    
    * [FIX] dependency version
    
    * test
    
    * test
    
    * test
    
    * test
    
    * [FIX]컴포넌트 정리
    
    * [FIX]textarea/px 수정
    
    * 컴포넌트간 props 전달 테스트
    
    * [ADD]헤더 추가
    
    * [FIX]Header fix
    
    * [ADD] router basename
    
    * [FIX]헤더 프레임 수정
    
    * [FIX]헤더 디자인 수정
    
    * [ADD]헤더 로그인 디자인 추가
    
    * [ADD]질문페이지 디테일 추가, 헤더 프로필 추가
    
    * 헤더 css 분리
    
    * 질문상세 props전달 테스트
    
    * [HOTFIX]
    
    ---------
    
    Co-authored-by: lavi02 <hoil.jeong@fleetune.com>
    Co-authored-by: lavi02 <18ds10535@dshs.kr>

---
+++ src/components/Question.jsx
@@ -0,0 +1,31 @@
+// @ts-nocheck
+import React from "react";
+import { useState } from "react";
+import { styles } from "./qdetail/style";
+import Dropdown from "./Dropdown";
+
+function Question() {
+    const [isChild, setIsChiled] = useState(true);
+
+    return (
+        <div style={styles.question_container}>
+            <div style={styles.q_box}>
+                <div style={styles.question_main}>
+                    <div style={styles.profile_box}><div style={styles.profile_img}></div></div>
+                    <div style={styles.question_main2}>
+                        <div style={styles.question_main3}>
+                            <div style={styles.question_hashtag}>#해시태그</div>
+                            <div style={{ flex: '1' }}>{isChild ? "어린이" : null}</div>
+                            <Dropdown />
+                        </div>
+                        <h3 style={styles.question_title}>질문 제목</h3>
+                        <div style={{ flex: '1' }}>🕓1시간 전</div>
+                    </div>
+                </div>
+                <div style={{ flex: '3', margin: '15px' }}>질문내용</div>
+            </div>
+        </div>
+    )
+}
+
+export default Question;
---
+++ src/components/qdetail/Answer.jsx
@@ -0,0 +1,58 @@
+// @ts-nocheck
+import React from "react";
+import { styles } from "./style";
+import { useState } from "react";
+import Newanswer from "./Newanswer";
+import Dropdown from "../Dropdown";
+import Reanswer from "../Reanswer";
+
+function Answer({ content, author, reply }) {//props로 답변 내용을 전달받음
+    const [view, setView] = useState(false);
+    const [answerOfAnswer, setAnswerOfAnswer] = useState(false);
+    const [isBtnClicked, setIsBtnClicked] = useState(false);
+    const [isBlurred, setIsBlurred] = useState(true);
+    const [reanswerList, setReanswerList] = useState(reply);
+
+    const handleCheckBtn = () => {
+        setIsBtnClicked(true);
+        setAnswerOfAnswer(true);
+        setIsBlurred(false);
+    };
+
+    return (
+        <div style={{ position: 'relative' }}>
+            {isBtnClicked ? null : (
+                <button onClick={handleCheckBtn} style={{ order: '-1', position: 'absolute', top: '50%', left: '42%', ...styles.answer_button, zIndex: '1000' }}>
+                    눌러서 답변 확인
+                </button>
+            )}
+            <div style={styles.question_main}>
+                <div style={styles.profile_box}><div style={styles.profile_img}></div></div>
+                <div style={styles.question_main2}>
+                    <div style={styles.question_main3}>
+                        <Dropdown />
+                    </div>
+                    <h3 style={styles.question_title}>{author}</h3>
+                </div>
+            </div>
+            <div style={{ margin: '15px', minHeight: '10vh', filter: isBlurred ? 'blur(5px)' : 'none' }}>
+                {content}
+            </div>
+
+            {answerOfAnswer && (
+                <div>
+                    {answerOfAnswer && (
+                        <div>
+                            {reanswerList.map((reanswer, index) => reanswer.content && reanswer.content.length > 0 ? (
+                                <Reanswer key={index} reply={reanswerList} content={reanswer.content} author={reanswer.author} />
+                            ) : null)}
+                            <Newanswer />
+                        </div>
+                    )}
+                </div>
+            )}
+        </div >
+    );
+}
+
+export default Answer;
