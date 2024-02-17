87706391efbce27757f9fcba094a0986ac139259
Baetato <diana021015@gmail.com>
2024-02-18 01:54:36 +0900

    메인페이지 연동 완료

--- src/Qdummy.jsx
+++ src/Qdummy.jsx
@@ -5,9 +5,9 @@ import { styles } from "src/components/MainPageDetail/style";
 import answericon from 'src/default.svg';
 import panswericon from 'src/professor.svg';
 
-function Qdummy({ profileImg }) {
+function Qdummy({title, answerCount, expertCount, createdAt, hashtag, user, childStatus, profileImg }) {
     const [isHovered, setIsHovered] = useState(false);
-
+    const childTag = childStatus === 'ACTIVE' ? '어린이' : '어른이';
     const handleMouseEnter = () => {
         setIsHovered(true);
       };
@@ -15,7 +15,6 @@ function Qdummy({ profileImg }) {
       const handleMouseLeave = () => {
           setIsHovered(false);
       };
-
     const defaultQdummy = { 
         color:"black",
         fontFamily: "Pretendard",
@@ -48,26 +47,32 @@ function Qdummy({ profileImg }) {
                     <div style={hoverQdummy}>
                         <div style={styles.contents}>
                             <img src={ profileImg } style={styles.profile}></img>
-                            <div class="level" style={{fontSize:"16px", marginTop:"28px", marginLeft:"209px"}}>
-                                <h5>어린이</h5>
+                            <div class="level" style={{fontSize:"16px", marginTop:"28px", marginLeft:"200px"}}>
+                                {/* <h5>어린이</h5> */}
+                                {childTag}
                             </div>
                             <div class="date" style={{fontSize:"13px", marginTop:"62px"}}>
-                                <h5>2023. 12. 20. 8:00am</h5>
+                                {/* <h5>2023. 12. 20. 8:00am</h5> */}
+                                {createdAt}
                             </div>
                             <div class="title" style={{fontSize:"20px", fontWeight:700, marginTop:"10px"}}>
-                                <p>현재 아르테미스 계획은</p>
-                                <p>어떻게 되어 가고 있나요?</p>
+                                {/* <p>현재 아르테미스 계획은</p>
+                                <p>어떻게 되어 가고 있나요?</p> */}
+                                {title}
                             </div>
                             <div class="reply" style={styles.answer}>
                                 <img src={answericon}></img>
-                                <span style={{marginLeft:"8px"}}>2</span>
+                                <span style={{marginLeft:"8px"}}>   </span>
+                                {answerCount}
                                 <img src={panswericon} style={{marginLeft:"8px"}}></img>
-                                <span style={{marginLeft:"8px"}}>3</span>
+                                <span style={{marginLeft:"8px"}}>   </span>
+                                {expertCount}
                             </div>
-                            <div class="tag" style={{fontSize:"15px", marginTop:"20px"}}>
-                                <span>#해시태그</span>
+                            <div class="tag" style={{fontSize:"15px", marginTop:"24%"}}>
+                                {/* <span>#해시태그</span>
                                 <span style={{marginLeft:"17px"}}>#해시태그</span>
-                                <span style={{marginLeft:"17px"}}>#해시태그</span>
+                                <span style={{marginLeft:"17px"}}>#해시태그</span> */}
+                                <span>#</span>{hashtag}
                             </div>
                         </div>
                     </div>
@@ -75,15 +80,18 @@ function Qdummy({ profileImg }) {
                     <div style={defaultQdummy}>
                         <div style={styles.contents}>
                             <img src={ profileImg } style={styles.profile}></img>
-                            <div class="level" style={{fontSize:"16px", marginTop:"28px", marginLeft:"209px", color:"#EB7125"}}>
-                                <h5>어린이</h5>
+                            <div class="level" style={{fontSize:"16px", marginTop:"28px", marginLeft:"200px", color:"#EB7125"}}>
+                                {/* <h5>어린이</h5> */}
+                                {childTag}
                             </div>
                             <div class="date" style={{fontSize:"13px", marginTop:"62px"}}>
-                                <h5>2023. 12. 20. 8:00am</h5>
+                                {/* <h5>2023. 12. 20. 8:00am</h5> */}
+                                {createdAt}
                             </div>
                             <div class="title" style={{fontSize:"20px", fontWeight:700, marginTop:"10px"}}>
-                                <p>현재 아르테미스 계획은</p>
-                                <p>어떻게 되어 가고 있나요?</p>
+                                {/* <p>현재 아르테미스 계획은</p>
+                                <p>어떻게 되어 가고 있나요?</p> */}
+                                {title}
                             </div>
                             {/* <div class="reply" style={styles.answer}>
                                 <img src={answericon}></img>
@@ -91,10 +99,11 @@ function Qdummy({ profileImg }) {
                                 <img src={panswericon} style={{marginLeft:"8px"}}></img>
                                 <span style={{marginLeft:"8px"}}>3</span>
                             </div> */}
-                            <div class="tag" style={{fontSize:"15px", marginTop:"109px"}}>
-                                <span>#해시태그</span>
+                            <div class="tag" style={{fontSize:"15px", marginTop:"60%"}}>
+                                {/* <span>#해시태그</span>
                                 <span style={{marginLeft:"17px"}}>#해시태그</span>
-                                <span style={{marginLeft:"17px"}}>#해시태그</span>
+                                <span style={{marginLeft:"17px"}}>#해시태그</span> */}
+                                <span>#</span>{hashtag}
                             </div>
                         </div>
                     </div>
