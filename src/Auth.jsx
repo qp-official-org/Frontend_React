c5f6d6a2445acaf3d093a835273c21deabba2d61
Baetato <diana021015@gmail.com>
2024-02-18 20:35:56 +0900

    간편 로그인 수정

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -2,60 +2,10 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import querystring from 'querystring';
 
 function Auth() {
 const [code, setCode] = useState({})
-
-useEffect(() => {
-    // Query String Parsing
-    let href = window.location.href;
-    // Parsing Query String
-    let queryString = href.substring(href.indexOf("?") + 1);
-    // Parsing Query String to Object
-    let queryObj = {};
-
-    // Split by "&"
-    queryString.split("&").forEach((value) => {
-        let key = value.split("=")[0];
-        let val = value.split("=")[1];
-        queryObj[key] = val;
-    });
-
-    let kakaoCode = queryObj["code"];
-
-    let data = {
-        "grant_type" : "authorization_code",
-        "client_id" : "1d5868c6919c5a981e66913f02262514",
-        "redirect_uri" : "http://localhost:3000/auth/kakao/login",
-        "code" : kakaoCode,
-    };
-
-    console.log(data);
-    axios.post("https://kauth.kakao.com/oauth/token", data,{
-        headers : {
-            "Content-type" : "application/x-www-form-urlencoded"
-        }
-    })
-    // .then(console.log("3")) 테스트 
-    .then((res) => {
-        console.log(res.data);
-				// 여기에 회원가입 요청 API 호출!
-        axios.get(`http://52.78.248.199:8080/users/sign_up?accessToken=${res.data.access_token}`);
-        setCode(res.data);
-    }).catch((err) => {
-        console.log(err);
-    });
-}, []);
-
-return (
-    <div>
-        <h1>AuthPage</h1>
-        {JSON.stringify(code)}
-    </div>
-);
-}
-// const [code, setCode] = useState({})
-// const navigate = useNavigate();
 
 // useEffect(() => {
 //     // Query String Parsing
@@ -81,45 +31,95 @@ return (
 //         "code" : kakaoCode,
 //     };
 
-//     console.log(data);
-//     axios.post("https://kauth.kakao.com/oauth/token", data,{
+//     console.log("1",data);
+//     // 쿼리스트링으로 데이터 넘기기 시도
+//     axios.post(`https://kauth.kakao.com/oauth/token`, querystring.stringify(data),{
 //         headers : {
 //             "Content-type" : "application/x-www-form-urlencoded"
 //         }
 //     })
+//     // .then(console.log("3")) 테스트
 //     .then((res) => {
 //         console.log(res.data);
+// 		// 여기에 회원가입 요청 API 호출!
+//         axios.get(`http://52.78.248.199:8080/users/sign_up?accessToken=${res.data.access_token}`);
 //         setCode(res.data);
-//         // 회원가입 API 호출
-//         return axios.get(`http://52.78.248.199:8080/users/sign_up?accessToken${res.data.access_token}`);
-//     })
-//     .then((res) => {
-//         // 회원가입 응답을 받아 사용자 ID 저장
-//         const userId = res.data.result.userId;
-//         // 유저 정보 조회 API 호출
-//         return axios.get(`http://52.78.248.199:8080/users/${userId}`, { headers: { "Authorization": `Bearer ${res.data.access_token}` }});
-//     })
-//     .then((res) => {
-//         // 유저 정보 조회 응답을 받아 사용자의 닉네임 확인
-//         if (!res.data.result.nickname) {
-//             // 닉네임이 없는 사용자라면 닉네임 설정 페이지로 이동
-//             navigate('/Nickname');
-//         } else {
-//             // 닉네임이 있는 사용자라면 메인 페이지로 이동
-//             navigate('/Main');
-//         }
-//     })
-//     .catch((err) => {
+//     }).catch((err) => {
 //         console.log(err);
 //     });
 // }, []);
 
-// return (
-//     <div>
-//         <h1>AuthPage</h1>
-//         {JSON.stringify(code)}
-//     </div>
-// );
-// }
+useEffect(() => {
+    // Query String Parsing
+    let href = window.location.href;
+    // Parsing Query String
+    let queryString = href.substring(href.indexOf("?") + 1);
+    // Parsing Query String to Object
+    let queryObj = {};
+
+    // Split by "&"
+    queryString.split("&").forEach((value) => {
+        let key = value.split("=")[0];
+        let val = value.split("=")[1];
+        queryObj[key] = val;
+    });
+
+    let kakaoCode = queryObj["code"];
+
+    // 현재 페이지의 URL을 가져온다.
+
+    let data = {
+        "grant_type" : "authorization_code",
+        "client_id" : "9c0435350e0714d02ef07e6bccb168ab",
+        "redirect_uri" : "http://15.164.53.109:3000/KakaoLogin/auth.html",
+        "code" : kakaoCode,
+    };
+
+    console.log(data);
+    axios.post("https://kauth.kakao.com/oauth/token", data,{
+        headers : {
+            "Content-type" : "application/x-www-form-urlencoded"
+        }
+    })
+        .then((res) => {
+            console.log(res.data);
+            axios.get(`http://52.78.248.199:8080/users/sign_up?accessToken=${res.data.access_token}`);
+            setCode(res.data);
+    }).catch((err) => {
+        console.log(err);
+    });
+}, []);
+
+return (
+    <div>
+        <h1>AuthPage</h1>
+        {JSON.stringify(code)}
+    </div>
+);
+}
+
+// useEffect(  async () => {
+//     const location = window.location;
+
+//     const params = new URLSearchParams(location.search);
+
+//     let code = params.get("code");
+
+//     let data = {
+//         "grant_type" : "authorization_code",
+//         "client_id" : "9c0435350e0714d02ef07e6bccb168ab",
+//         code,
+//     };
+
+//     console.log(data);
+//     let response = await axios.post("https://kauth.kakao.com/oauth/token", data, {
+//         headers: {
+//             "Content-type": "application/x-www-form-urlencoded"
+//         }
+//     });
+
+//     console.log(response.data);
+
+// }, []);
 
 export default Auth;
